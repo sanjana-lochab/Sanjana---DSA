11a35c6cd756bc9fc0c43cc24ef5c433a518530a
sanjana lochab <sanjanalochab7146@gmail.com>
2026-07-11 15:39:17 +0530

    Time: 0 ms (100%), Space: 8.5 MB (37.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,12 +10,17 @@ public:
     }
     // usig bottom up approch
     int solveusingBUApproch(int n, vector<int> & dp){
-        dp[0]= 1;
-        dp[1] = 1;
+        if(n==0) return 1;
+        if(n==1) return 1;
+        int prev2 = 1;
+        int prev1 = 1;
+        int curr = -1;
         for(int i = 2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            curr= prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        return curr;
     }
     int climbStairs(int n) {
         
