c98c111f945118b4e8b1ff10531f37fbeec854a6
sanjana lochab <sanjanalochab7146@gmail.com>
2026-08-25 16:26:49 +0530

    Time: 0 ms (100%), Space: 11.5 MB (79.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,11 +3,14 @@ public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
         for(int i = 0;i<n-1;i++){
+            bool swaped = false;
             for(int j = 0;j<n-i-1;j++){
                 if(nums[j]>nums[j+1]){
                     swap(nums[j],nums[j+1]);
+                    swaped = true;
                 }
             }
+            if(!swaped) break;
         }
     }
 };
